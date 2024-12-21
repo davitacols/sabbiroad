47ae61cc7604cadefe2014ecb612549fde33f224
davitacols <davitacols@gmail.com>
2024-12-21 18:14:02 +0100

    disable lints

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 export default {
+  eslint: {
+    ignoreDuringBuilds: true,  // Disables linting during build
+  },
     webpack: (config) => {
       config.module.rules.push({
         test: /\.html$/,
